7aa938df6c4cf3caf92812870ab2a1e0616672b7
JBBDC <matan4bb@gmail.com>
2019-10-19 15:07:24 +0500

    add meals table

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -9,3 +10,15 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories, user_id) VALUES
+('2019-10-17 09:00:00', 'breakfast', 500, 100000),
+('2019-10-17 13:00:00', 'lunch', 1000, 100000),
+('2019-10-17 18:00:00', 'dinner', 1000, 100000),
+('2019-10-18 10:00:00', 'first', 700, 100000),
+('2019-10-18 14:00:00', 'second', 600, 100000),
+('2019-10-18 22:00:00', 'third', 500, 100000),
+('2019-10-18 07:00:00', 'i', 10, 100001),
+('2019-10-18 12:00:00', 'am', 15, 100001),
+('2019-10-18 17:00:00', 'a', 20, 100001),
+('2019-10-18 22:00:00', 'bird', 23, 100001)
