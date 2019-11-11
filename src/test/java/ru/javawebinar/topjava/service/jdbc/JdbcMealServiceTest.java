7b7bfc3f853a4e5c956fbaff70f5a6316a702de6
JBBDC <matan4bb@gmail.com>
2019-11-11 11:52:27 +0500

    fixed jdbc tests with Assume and Enviroment

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
@@ -1,18 +1,26 @@
 package ru.javawebinar.topjava.service.jdbc;
 
-import org.junit.Ignore;
+import org.junit.Assume;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.service.AbstractMealServiceTest;
+
+import java.util.Arrays;
 
 import static ru.javawebinar.topjava.Profiles.JDBC;
 
 @ActiveProfiles(JDBC)
 public class JdbcMealServiceTest extends AbstractMealServiceTest {
 
-    @Ignore
+    @Autowired
+    private Environment environment;
+
     @Override
     @Test
     public void createWithException() throws Exception {
+        Assume.assumeFalse(Arrays.asList(environment.getActiveProfiles()).contains(JDBC));
+        super.createWithException();
     }
 }
--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
@@ -1,18 +1,27 @@
 package ru.javawebinar.topjava.service.jdbc;
 
-import org.junit.Ignore;
+import org.junit.Assume;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+
+import java.util.Arrays;
 
 import static ru.javawebinar.topjava.Profiles.JDBC;
 
 @ActiveProfiles(JDBC)
 public class JdbcUserServiceTest extends AbstractUserServiceTest {
 
-    @Ignore
+    @Autowired
+    private Environment environment;
+
     @Override
     @Test
     public void createWithException() throws Exception {
+        Assume.assumeFalse(Arrays.asList(environment.getActiveProfiles()).contains(JDBC));
+        super.createWithException();
     }
 }
+
